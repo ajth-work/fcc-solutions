1de70fd8bffeae69197b0678601562d9bc3bd043
Andrew-John Hickman <60426517+ajth-work@users.noreply.github.com>
2020-05-08 16:21:27 +0900

    Add files via upload

---
+++ solutions/javascript-algorithms-and-data-structures/Intermediate Algorithm Scripting/drop-it.js
@@ -0,0 +1,19 @@
+function dropElements(arr, func) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i]) == true) {
+      newArr = arr.splice(i, arr.length);
+    };
+  }
+  console.log(newArr)
+  return newArr;
+}
+
+dropElements([1, 2, 3], function (n) { return n < 3; }); // should return [1, 2].
+dropElements([1, 2, 3, 4], function (n) { return n >= 3; }); // should return [3, 4].
+dropElements([0, 1, 0, 1], function (n) { return n === 1; }); // should return [1, 0, 1].
+dropElements([1, 2, 3], function (n) { return n > 0; }); // should return [1, 2, 3].
+
+dropElements([1, 2, 3, 4], function (n) { return n > 5; }); // should return [].
+dropElements([1, 2, 3, 7, 4], function (n) { return n > 3; }); // should return [7, 4].
+dropElements([1, 2, 3, 9, 2], function (n) { return n > 2; }); // should return [3, 9, 2]
