777e8f3fa996bf0e5182dce813c6ef8b85998118
ajth-work <ajth.work@gmail.com>
2020-05-12 04:22:30 +0900

    convert from .txt to .jsx files

---
+++ solutions/front-end-libraries/React/create-a-complex-jsx-element.jsx
@@ -0,0 +1,11 @@
+// write your code here
+const JSX =
+  <div>
+    <h1>Main Text</h1>
+    <p>Details about Main Text</p>
+    <ul>
+      <li>Introductory Information</li>
+      <li>Primary Information</li>
+      <li>Concluding Information</li>
+    </ul>
+  </div>;
---
+++ solutions/front-end-libraries/React/create-a-stateless-functional-component.jsx
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+  // change code below this line
+return (
+  <div> Hello World! </div>
+)
+
+
+  // change code above this line
+}
